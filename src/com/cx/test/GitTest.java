0fc1f90552e3b49653c92288714c4e7f7d85366c
chenxi <1264902690@qq.com>
2021-05-30 21:08:04 +0800

    测试2

--- src/com/cx/test/GitTest.java
+++ src/com/cx/test/GitTest.java
@@ -4,5 +4,6 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello gits");
         System.out.println("hello gits1");
+        System.out.println("hello gits2");
     }
 }
