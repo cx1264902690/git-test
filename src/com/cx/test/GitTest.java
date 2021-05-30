bfbc2c1c2dab822445293a67714f5b6c17437488
chenxi <1264902690@qq.com>
2021-05-30 21:19:47 +0800

    hot-fix 1

--- src/com/cx/test/GitTest.java
+++ src/com/cx/test/GitTest.java
@@ -3,5 +3,7 @@ package com.cx.test;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello gits");
+        System.out.println("hello gits4");
+        System.out.println("hello gits5");
     }
 }
